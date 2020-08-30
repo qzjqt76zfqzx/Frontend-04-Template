6ae43aa0e47175ee604b618eaf0149992cd09ea7
John Doe <your_email@example.com>
2020-08-29 21:12:10 -0400

    feat(*): add 'O' and 'X' step one by one v1.3.0

--- Week_01/src/js/index.js
+++ Week_01/src/js/index.js
@@ -4,15 +4,27 @@ let pattern = [
   [0, 0, 0],
 ];
 
+let color = 1;
+const LOGIC_NUMBER = 3;
+
 function show() {
   let board = document.getElementById("board");
+  board.innerHTML = ""; //always let board empty before show
+
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell"); //add class "cell" to new div
       cell.innerText = pattern[i][j] == 1 ? "O" : pattern[i][j] == 2 ? "X" : "";
+      cell.addEventListener("click", () => move(i, j));
       board.appendChild(cell); //add cell to board
     }
     board.appendChild(document.createElement("br")); //change to new line
   }
+}
+
+function move(x, y) {
+  pattern[x][y] = color;
+  color = LOGIC_NUMBER - color;
+  show();
 }
