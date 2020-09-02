616a0f79d7b3a3cad64e952540b13d2898445b56
John Doe <your_email@example.com>
2020-09-02 00:10:38 -0400

    feat(*): add AI for win-lost check

--- Week_01/src/js/index.js
+++ Week_01/src/js/index.js
@@ -33,8 +33,14 @@ function move(x, y) {
         ? (document.getElementById("result").innerHTML = "O win")
         : (document.getElementById("result").innerHTML = "X win");
     }
-    willWin(pattern, color);
+    if (willWin(pattern, color).result) {
+      color == 1
+        ? (document.getElementById("hint").innerHTML = "O will win")
+        : (document.getElementById("hint").innerHTML = "X will win");
+    }
+
     color = LOGIC_NUMBER - color;
+    console.log(bestChoice(pattern, color)); 
   }
 }
 
@@ -105,13 +111,38 @@ function willWin(pattern, color) {
         let tmp = clone(pattern);
         tmp[i][j] = color;
         if (check(tmp, color)) {
-          color == 1
-            ? (document.getElementById("hint").innerHTML = "O will win")
-            : (document.getElementById("hint").innerHTML = "X will win");
-          return true;
+          return { result: true, position: [i, j] };
         }
       }
     }
   }
-  return false;
+  return { result: false };
+}
+
+function bestChoice(pattern, color) {
+  let p;
+  if ((p = willWin(pattern, color).result)) {
+    return { point: p, result: 1 };
+  }
+
+  let result = -2;
+  let point = null;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (pattern[i][j]) continue;
+      let tmp = clone(pattern);
+      tmp[i][j] = color;
+      let r = bestChoice(tmp, 3 - color).result;
+
+      if (-r > result) {
+        result = -r;
+        point = [i, j];
+      }
+    }
+  }
+
+  return {
+    point: point,
+    result: point ? result : 0,
+  };
 }
